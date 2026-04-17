6b3b2dff9809207fdc4989805d52e54f4cf43c5e
zaqimaulana <zaqimaulana72@gmail.com>
2026-04-17 18:46:14 +0700

    membuat google sign in button

---
+++ lib/core/widgets/google_sign_in_button.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class GoogleSignInButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final bool isLoading;
+
+  const GoogleSignInButton({
+    super.key,
+    this.onPressed,
+    this.isLoading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 52,
+      child: OutlinedButton(
+        onPressed: isLoading ? null : onPressed,
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.white,
+          side: BorderSide(color: Colors.grey.shade300),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: isLoading
+            ? const SizedBox(
+                width: 20,
+                height: 20,
+                child:
+                    CircularProgressIndicator(strokeWidth: 2),
+              )
+            : Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/icons/google_logo.png',
+                    height: 22,
+                  ),
+                  const SizedBox(width: 12),
+                  const Text(
+                    'Lanjutkan dengan Google',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black87,
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
