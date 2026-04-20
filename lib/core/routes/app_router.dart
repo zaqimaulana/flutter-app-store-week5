5d14f09d2437e575178d56a4d81f2cf83c40463e
zaqimaulana <zaqimaulana72@gmail.com>
2026-04-21 00:47:07 +0700

    membuat router app

---
+++ lib/core/routes/app_router.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../../features/auth/presentation/pages/login_page.dart';
+import '../../features/auth/presentation/pages/register_page.dart';
+import '../../features/auth/presentation/pages/verify_email_page.dart';
+import '../../features/dashboard/presentation/pages/dashboard_page.dart';
+import '../../features/splash/presentation/pages/splash_page.dart';
+
+import 'auth_guard.dart';
+
+class AppRouter {
+  static const String splash      = '/';
+  static const String login       = '/login';
+  static const String register    = '/register';
+  static const String verifyEmail = '/verify-email';
+  static const String dashboard   = '/dashboard';
+
+  static Map<String, WidgetBuilder> get routes => {
+    splash:      (_) => const SplashPage(),
+    login:       (_) => const LoginPage(),
+    register:    (_) => const RegisterPage(),
+    verifyEmail: (_) => const VerifyEmailPage(),
+    dashboard:   (_) => const AuthGuard(child: DashboardPage()),
+  };
+}
