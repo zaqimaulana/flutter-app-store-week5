c5ed4b25c59717c05869475dbda18c8ecec802a9
zaqimaulana <zaqimaulana72@gmail.com>
2026-04-23 16:48:53 +0700

    add backend submodule

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,9 +1,9 @@
-class ApiConstants { 
-  static const String baseUrl = 'http://10.0.2.2:8081/v1'; 
-  
-  static const String verifyToken = 'auth/verify-token'; 
-  static const String products = 'products'; 
-  
-  static const int connectTimeout = 15000; 
-  static const int receiveTimeout = 15000; 
+class ApiConstants {
+  static const String baseUrl = 'http://127.0.0.1:8081/v1';
+
+  static const String verifyToken = '/auth/verify-token';
+  static const String products = '/products';
+
+  static const int connectTimeout = 15000;
+  static const int receiveTimeout = 15000;
 }
